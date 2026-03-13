c7ac9283d639f4a67c768b7b8495e67b478cd17c
vencyradadiya16 <vencyradadiya16@gmail.com>
2026-03-13 20:13:30 +0530

    Practical 8 Dynamic Pattern  code

--- PatternGenerator.java
+++ PatternGenerator.java
@@ -2,15 +2,32 @@ import java.util.Scanner;
 
 public class PatternGenerator {
 
-    // Method for Right Triangle Pattern
+    // Method for Right Angle Pattern
     public static void rightTriangle(char symbol, int rows) {
-
         System.out.println("\nRight Triangle Pattern:\n");
 
         for (int i = 1; i <= rows; i++) {
-
             for (int j = 1; j <= i; j++) {
                 System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Method for Full Pyramid Pattern
+    public static void fullPyramid(char symbol, int rows) {
+        System.out.println("\nFull Pyramid Pattern:\n");
+
+        for (int i = 1; i <= rows; i++) {
+
+            // Spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+
+            // Symbols
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print(symbol);
             }
 
             System.out.println();
@@ -28,6 +45,7 @@ public class PatternGenerator {
         int rows = sc.nextInt();
 
         rightTriangle(symbol, rows);
+        fullPyramid(symbol, rows);
 
         sc.close();
     }
